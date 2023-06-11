280dfe990dc6beee3fd45588f4782450cdf3427e
7Soldier <reg.fm4@gmail.com>
2023-06-11 12:02:52 +0000

    RMX2151: Rebrand to TWRP
    Co-authored-by: Carlo Dee <109793453+carlodeeCrypton@users.noreply.github.com>
    Co-authored-by: sekaiacg <sekaiacg@gmail.com>
    Co-authored-by: TheMalachite <eliasgheeraert@gmail.com>
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- twrp_RMX2151.mk
+++ twrp_RMX2151.mk
@@ -7,16 +7,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from RMX2151 device
 $(call inherit-product, device/realme/RMX2151/device.mk)
 
 PRODUCT_DEVICE := RMX2151
-PRODUCT_NAME := omni_RMX2151
+PRODUCT_NAME := twrp_RMX2151
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme 7
 PRODUCT_MANUFACTURER := realme
